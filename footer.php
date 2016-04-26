59f2ec5732d24c94b4ef0ce64412649fdae09bb3
Casey <info@caseyburton.com>
2016-04-26 15:43:55 -0700

    initial

--- footer.php
+++ footer.php
@@ -14,10 +14,10 @@
 
 <div id="footer-container">
 			<footer id="footer">
-				<blockquote class="quote">
+				<!-- <blockquote class="quote">
 					<p><strong>In the time of your life, live &mdash;</strong> so that in that wondrous time you shall not add to the misery <span class="amp">&amp;</span> sorrow of the world, but shall smile to the infinite delight and mystery of it.</p>
 					<cite> William Saroyan</cite>
-				</blockquote>
+				</blockquote> -->
 				<!-- <?php do_action( 'foundationpress_before_footer' ); ?>
 				<?php dynamic_sidebar( 'footer-widgets' ); ?>
 				<?php do_action( 'foundationpress_after_footer' ); ?> -->
--- page-templates/front.php
+++ page-templates/front.php
@@ -7,15 +7,9 @@ get_header(); ?>
 <header id="front-hero" role="banner">
 	<div class="marketing">
 		<div class="tagline">
-			<h1 itemscope itemtype="http://schema.org/Person" class="name"><span itemprop="givenName" class="givenname">Casey</span> <span itemprop="familyName" class="familyname">Burton</span></h1>
-			<ul class="subheader">
-				<li>Husband<small> to a beautiful woman full of <a href="http://www.thedancestudio.net" title="Go check out The Dance Studio - teaching a love for dance since 1979" target="blank">dance</a> and joy,</small></li>
-				<li><small>dedicated </small>small business owner, </li>
-				<li><small>dance studio handyman / plumber extrordinaire,</small></li>
-				<li>graphic <i class="fa fa-plus" aria-hiden="true" title="plus"><span class="sr-only">plus</span></i>web designer,</li>
-				<li><small>son-of-a-fisherman,</small></li>
-				<li><span class="amp">&amp;</span> <small>all-around</small> persnickety bastard.</li>
-			</ul>
+			<p class="branding"><span class="happy">Happy</span><br> <span class="chap">Chap</span></p>
+			<p class="brand-tag"><span class="creative">Creative</span> <span class="co">Co</span></p>
+			<h1 class="subheader">A PNW design, build, create, make, help <span class="amp">&amp;</span> strategize agency. Digital <span class="amp">&amp;</span> <strike>dead tree</strike> print.</h1>
 			<p class="location">Located in beautiful <strong>Bellingham, WA</strong>.</p>
 			<!-- <p><a href="#" class="button large hollow"><i class="fa fa-envelope"></i>Get at me</a></p> -->
 		</div>
