076436cddca1bbeb1439b27241e95a4aa68dacf7
Casey <info@caseyburton.com>
2016-05-03 02:00:17 -0700

    initial styling

--- footer.php
+++ footer.php
@@ -13,10 +13,25 @@
 		</section>
 
 <div id="footer-container">
-			<footer id="footer">
+			<!-- <footer id="footer">
 				<h2 class="tease-header">Soon&hellip;</h2>
-				<p class="tease-body">Exciting things are happening with <strong>Los Chapos</strong> &ndash; stay tuned folks!</p>
-			</footer>
+				<p class="tease-body">Exciting things are happening with <strong>The Chaps</strong> &ndash; stay tuned folks!</p>
+				Begin MailChimp Signup Form
+				<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+				<div id="mc_embed_signup">
+				<form action="//happychapmedia.us10.list-manage.com/subscribe/post?u=82b0af98035b46118794c1b6f&amp;id=eb75f38c63" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					<label for="mce-EMAIL">Put your email here:</label>
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+				    real people should not fill this in and expect good things - do not remove this or risk form bot signups
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_82b0af98035b46118794c1b6f_eb75f38c63" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" value="Stalk Us" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				    </div>
+				</form>
+				</div>
+			
+				End mc_embed_signup
+			</footer> -->
 		</div>
 
 		<?php do_action( 'foundationpress_layout_end' ); ?>
--- page-templates/front.php
+++ page-templates/front.php
@@ -19,11 +19,44 @@ get_header(); ?>
 					<?php echo do_shortcode('[contact-form-7 id="11" title="main-TEMP"]'); ?>
 			    </div>
 			  </li>
-			  <!-- ... -->
 			</ul>
 		</div>
 	</div>
 
 </header>
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+ <?php while ( have_posts() ) : the_post(); ?>
+   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+       <div class="entry-content">
+      		<h2>Things we'll do</h2>
+      		<p class="subheader">For awesome clients who are keen to the value of thoughtful work.</p>
+       		<ul class="services">
+       			<li>Branding</li>
+       			<li>Websites</li>
+       			<li>Social Media</li>
+       			<li>Strategy</li>
+       			<li>Signs</li>
+       			<li>PR</li>
+       			<li>Apperal</li>
+       			<li>Posters</li>
+       			<li>Menus</li>
+       			<li>Advertising</li>
+       			<li>Consulting</li>
+       			<li>Packaging</li>
+       			<li>Propaganda</li>
+       			<li>Flyers</li>
+       			<li>&amp; more</li>
+       		</ul>
+       		<h2>Things we've done</h2>
+       		<p class="subheader">For all sorts of businesses and causes &mdash; and some random pictures, because we did.</p>
+           <?php the_content(); ?>
+           <p class="portfolio-cta">How can we help conspire to find smart, snappy &amp; effective solutions to your creative needs?</p>
+           <p class="temp-footer">Happy<span class="chap">Chap</span></p>
+           <p class="creative">Creative Co.</p>
+       </div>
+   </article>
+ <?php endwhile;?>
 
 <?php get_footer();
