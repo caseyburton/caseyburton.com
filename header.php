966ed84e6036bbd6bf55103d80930e731f90598b
Casey <info@caseyburton.com>
2016-05-04 10:00:00 -0700

    favicon

--- header.php
+++ header.php
@@ -22,12 +22,10 @@
 		    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 		  })(document);
 		</script>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-57x57.png">
 		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-60x60.png">
 		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_dire1ctory_uri(''); ?>/assets/images/icons/apple-touch-icon-76x76.png">
 		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-114x114.png">
 		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-120x120.png">
 		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-144x144.png">
@@ -42,7 +40,6 @@
 		<meta name="msapplication-TileColor" content="#da532c">
 		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/mstile-144x144.png">
 		<meta name="theme-color" content="#f9ef78">
-
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
