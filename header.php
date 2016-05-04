7d3dac418dcc53743a697e266eae702ffa4c1e7a
Casey <info@caseyburton.com>
2016-05-04 10:04:50 -0700

    extra charachter

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-57x57.png">
 		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-60x60.png">
 		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_dire1ctory_uri(''); ?>/assets/images/icons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-76x76.png">
 		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-114x114.png">
 		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-120x120.png">
 		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(''); ?>/assets/images/icons/apple-touch-icon-144x144.png">
